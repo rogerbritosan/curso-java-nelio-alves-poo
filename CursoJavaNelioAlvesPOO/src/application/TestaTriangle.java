f1bf2cf2e9e6a52e4667e9516dd14e24c503d706
Roger <rogerbritosan@gmail.com>
2023-01-30 18:37:37 -0300

    Editando o toString

---
+++ CursoJavaNelioAlvesPOO/src/application/TestaTriangle.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
+
+public class TestaTriangle {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		// values: 3.00, 4.00, 5.00
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of triangle Y: ");
+		// values: 7.50, 4.50, 4.02
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		
+		if(areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		sc.close();
+		
+		
+	}
+
+}
--- CursoJavaNelioAlvesPOO/src/application/TestaTriangleProcedural.java
+++ CursoJavaNelioAlvesPOO/src/application/TestaTriangleProcedural.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class TestaTriangleProcedural {
 	
 	public static void main(String[] args) {
 		
