ad55fed1a8f7b99cb6d2c9aecba968f41b4a0c73
Roger <rogerbritosan@gmail.com>
2023-02-01 15:37:26 -0300

    Array de objetos

---
+++ CursoJavaNelioAlvesPOO/src/application/VetoresParte1.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VetoresParte1 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.printf("Digite o número de pessoas: ");
+		int n = sc.nextInt();
+
+		double[] vect = new double[n];
+		// double vect[] = new double[n];
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.print("Digite a altura da " + (i + 1) + "ª pessoa: ");
+			vect[i] = sc.nextDouble();
+
+		}
+
+		double sum = 0.0;
+
+		for (Double p : vect) {
+			// System.out.printf("Altura: %.2f%n", p);
+
+			sum += p;
+
+		}
+		
+		double avg = sum / n;
+		
+		System.out.println("Average height: " + avg);
+
+		sc.close();
+
+	}
+
+}
---
+++ CursoJavaNelioAlvesPOO/src/application/VetoresParte2.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+import entities.Product6;
+
+public class VetoresParte2 {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a quantidade produtos: ");
+		int n = sc.nextInt();
+		
+		Product6[] vect = new Product6[n];
+		
+		double sum = 0.0;
+		double avg = 0.0;
+		
+		for(int i = 0; i < vect.length; i++) {
+			
+			sc.nextLine();
+			
+			System.out.print("Digite o nome do produto: ");
+			String name = sc.nextLine();
+			
+			System.out.print("Digite o preço do produto: ");
+			double price = sc.nextDouble();
+			
+			vect[i] = new Product6(name, price);
+			
+			sum += vect[i].getPrice();
+			avg = sum / vect.length;
+			
+		}
+		
+		System.out.printf("Average price: %.2f%n", avg);
+		
+		
+		
+		sc.close();
+	}
+
+}
