8b54aef2059fc32ec72cd454a97e628ea543e542
Roger <rogerbritosan@gmail.com>
2023-02-03 18:50:00 -0300

    Trabalhando com datas

---
+++ CursoJavaNelioAlvesPOO/src/application/DataHora.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class DataHora {
+
+	public static void main(String[] args) {
+		
+		// INstanciação
+		// ISO 8601
+		
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now();
+
+		LocalDate d04 = LocalDate.parse("2022-07-20");
+		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+		
+		System.out.println("d01 = " + d01.toString()); // chama implicitamente o toString() 
+		System.out.println("d02 = " + d02);
+		System.out.println("d03 = " + d03);
+		System.out.println("d04 = " + d04);
+		System.out.println("d05 = " + d05);
+		
+		
+
+	}
+
+}
